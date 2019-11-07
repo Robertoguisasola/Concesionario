09e178f0a88d20ddc907e32cb29bfa1985298ac7
Roberto <Roberto@portatil>
2019-11-07 18:15:36 +0100

    Corrección de la ventana login y adición de fallo en clave

--- ProyectoConcesionario/src/ventanas/VentanaLogin.java
+++ ProyectoConcesionario/src/ventanas/VentanaLogin.java
@@ -1,124 +1,90 @@
 package ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-
-import java.sql.*;
+import javax.swing.JTextField;
 
 import dataBase.GestorBD;
-
-
-import java.awt.Font;
 import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 
 public class VentanaLogin extends JFrame {
-
-	private JTextField textoUsuario;
-	private JPasswordField textoContrasenya;
-	private JButton botonIniciar;
-	private JButton botonRegistrar;
 	
-	public VentanaLogin() {
+	private JPanel formPanel;
+	private JLabel loginLabel;
+	private JTextField loginField;
+	private Box loginBox;
+	private JLabel passwordLabel;
+	private JPasswordField passwordField;
+	private Box passwordBox;
+	private Box formBox;
+	private JPanel buttonsPanel;
+	private Box buttonsBox;
+	private JButton acceptButton;
+	private JButton cancelButton;
+	private JLabel falloInicio;
+	
+	public VentanaLogin(){
+		this.setTitle("Login");
 		
-		getContentPane().setBackground(new Color(255, 255, 224));
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{99, 0, 0, 0, 97, 0, 45, 86, 0, 0};
-		gridBagLayout.rowHeights = new int[]{14, 0, 35, 20, 20, 36, 23, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(320, 240);
+		this.setResizable(true);
 		
-		JLabel lblLogin = new JLabel("LOGIN");
-		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
-		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
-		gbc_lblLogin.fill = GridBagConstraints.VERTICAL;
-		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLogin.gridx = 4;
-		gbc_lblLogin.gridy = 1;
-		getContentPane().add(lblLogin, gbc_lblLogin);
+		formPanel = new JPanel();
+		formPanel.setLayout(new GridBagLayout());
 		
-		JLabel lblUsuario = new JLabel("Usuario:");
-		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 11));
-		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
-		gbc_lblUsuario.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
-		gbc_lblUsuario.gridx = 1;
-		gbc_lblUsuario.gridy = 3;
-		getContentPane().add(lblUsuario, gbc_lblUsuario);
+		loginLabel = new JLabel("Login");
+		loginField = new JTextField(12);
 		
-		textoUsuario = new JTextField();
-		textoUsuario.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_textoUsuario = new GridBagConstraints();
-		gbc_textoUsuario.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textoUsuario.anchor = GridBagConstraints.NORTH;
-		gbc_textoUsuario.insets = new Insets(0, 0, 5, 5);
-		gbc_textoUsuario.gridwidth = 4;
-		gbc_textoUsuario.gridx = 3;
-		gbc_textoUsuario.gridy = 3;
-		getContentPane().add(textoUsuario, gbc_textoUsuario);
-		textoUsuario.setColumns(10);
+		loginBox = new Box(BoxLayout.X_AXIS);
+		loginBox.add(loginLabel);
+		loginBox.add(Box.createRigidArea(new Dimension(40,0)));
+		loginBox.add(loginField);
 		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
-		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 11));
-		GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
-		gbc_lblContrasea.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
-		gbc_lblContrasea.gridx = 1;
-		gbc_lblContrasea.gridy = 5;
-		getContentPane().add(lblContrasea, gbc_lblContrasea);
+		passwordLabel = new JLabel("Password");
+		passwordField = new JPasswordField(12);
 		
-		textoContrasenya = new JPasswordField();
-		textoContrasenya.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_textoContrasenya = new GridBagConstraints();
-		gbc_textoContrasenya.anchor = GridBagConstraints.NORTH;
-		gbc_textoContrasenya.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textoContrasenya.insets = new Insets(0, 0, 5, 5);
-		gbc_textoContrasenya.gridwidth = 4;
-		gbc_textoContrasenya.gridx = 3;
-		gbc_textoContrasenya.gridy = 5;
-		getContentPane().add(textoContrasenya, gbc_textoContrasenya);
+		passwordBox = new Box(BoxLayout.X_AXIS);
+		passwordBox.add(passwordLabel);
+		passwordBox.add(Box.createRigidArea(new Dimension(10,0)));
+		passwordBox.add(passwordField);
 		
-		botonRegistrar = new JButton("Registrar nuevo usuario");
-		botonRegistrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				/*VentanaRegistrar ventanaRegistrar = new VentanaRegistrar();
-				ventanaRegistrar.setVisible(true);
-				ventanaRegistrar.setSize(450,560);
-				ventanaRegistrar.setLocationRelativeTo(null);
-				
-				dispose();
-				*/
-				
-			}
-		});
-		botonRegistrar.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		GridBagConstraints gbc_botonRegistrar = new GridBagConstraints();
-		gbc_botonRegistrar.anchor = GridBagConstraints.NORTH;
-		gbc_botonRegistrar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_botonRegistrar.insets = new Insets(0, 0, 5, 5);
-		gbc_botonRegistrar.gridwidth = 3;
-		gbc_botonRegistrar.gridx = 1;
-		gbc_botonRegistrar.gridy = 7;
-		getContentPane().add(botonRegistrar, gbc_botonRegistrar);
+		falloInicio = new JLabel("Usuario o contraseña incorrecta");
+		falloInicio.setForeground(Color.RED);
+		falloInicio.setVisible(false);
 		
-		botonIniciar = new JButton("Iniciar");
-		botonIniciar.addActionListener(new ActionListener() {
+		formBox = new Box(BoxLayout.Y_AXIS);
+		formBox.add(loginBox);
+		formBox.add(Box.createRigidArea(new Dimension(0,20)));
+		formBox.add(passwordBox);
+		formBox.add(falloInicio);
+		
+		formPanel.add(formBox);
+		
+		buttonsPanel = new JPanel();
+		
+		buttonsBox = new Box(BoxLayout.X_AXIS);
+		cancelButton = new JButton("Cancelar");
+		acceptButton = new JButton("Aceptar");
+		buttonsPanel.add(acceptButton);
+		
+		acceptButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					GestorBD bd = new GestorBD();
-					String usuario = textoUsuario.getText();
-					String contra = new String(textoContrasenya.getPassword());
+					String usuario = loginLabel.getText();
+					String contra = new String(passwordField.getPassword());
 					
 					if (bd.iniciarSesionCliente(usuario, contra)) {
 						//TODO lanza ventana de cliente
@@ -126,6 +92,8 @@ public class VentanaLogin extends JFrame {
 						//TODO lanza ventana de trabajadores
 					}else if (usuario.equals("admin")&& contra.equals("1234")) {
 						//TODO lanza ventana de administrador
+					}else {
+						falloInicio.setVisible(true);
 					}
 					bd.desconectar();
 				} catch (Exception e) {
@@ -133,13 +101,14 @@ public class VentanaLogin extends JFrame {
 				}
 			}
 		});
-		GridBagConstraints gbc_botonIniciar = new GridBagConstraints();
-		gbc_botonIniciar.insets = new Insets(0, 0, 5, 5);
-		gbc_botonIniciar.anchor = GridBagConstraints.NORTH;
-		gbc_botonIniciar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_botonIniciar.gridx = 5;
-		gbc_botonIniciar.gridy = 7;
-		getContentPane().add(botonIniciar, gbc_botonIniciar);
+		buttonsBox.add(Box.createRigidArea(new Dimension(10,0)));
+		buttonsBox.add(cancelButton);
+				
+		buttonsPanel.add(buttonsBox);
 		
+		getContentPane().add(formPanel, BorderLayout.CENTER);
+		getContentPane().add(buttonsPanel,BorderLayout.SOUTH);
+		
+		this.setVisible(true);
 	}
 }
