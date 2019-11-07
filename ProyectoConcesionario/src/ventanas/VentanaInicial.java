aa6fb84b03b755f13bd76dc220e2a5fa6f2240a2
Roberto <Roberto@portatil>
2019-11-07 18:30:05 +0100

    eliminación imports innecesarios

--- ProyectoConcesionario/src/ventanas/VentanaInicial.java
+++ ProyectoConcesionario/src/ventanas/VentanaInicial.java
@@ -1,22 +1,14 @@
 package ventanas;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
-import dataBase.GestorBD;
 
 public class VentanaInicial extends JFrame {
 	//TODO enlazar ventana como principal
