affb0d4dffde0fe60376496487c443d8ea9a1501
Roberto <Roberto@TorreBilbao>
2019-12-01 17:46:47 +0100

    Ventana de coches creada

--- ProyectoConcesionario/src/ventanas/TablaCoches.java
+++ ProyectoConcesionario/src/ventanas/TablaCoches.java
@@ -1,7 +1,19 @@
 package ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+
+import model.Persona;
 
 public class TablaCoches extends JFrame {
 	
@@ -9,7 +21,14 @@ public class TablaCoches extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	private JScrollPane tablaPanel;
+	private JPanel botonesPanel;
+	private JButton anadirButton;
+	private JButton eliminarButton;
+	private JTable tabla;
+	
+	//TODO R terminar ventana
 	public TablaCoches() {
 		this.setTitle("Tabla de coches");
 		
@@ -18,7 +37,54 @@ public class TablaCoches extends JFrame {
 		this.setResizable(true);
 		this.setLocationRelativeTo(null);
 		
-		String[] columnas;
+		//TODO rellenar columnas
+		String[] columNames = {"Column1", "Column2"};
+		String[][] data = {
+				{"Nombre1", "Dato1"},
+				{"Nombre2", "Dato2"},
+		};
+		
+		tabla = new JTable(data, columNames);
+		
+		tablaPanel = new JScrollPane(tabla);
+		
+		
+		botonesPanel = new JPanel();
+		botonesPanel.setLayout(new GridBagLayout());
+
+		anadirButton = new JButton("Añadir");
+		botonesPanel.add(anadirButton);
+		
+		eliminarButton = new JButton("Eliminar");
+		botonesPanel.add(eliminarButton);
+		
+		anadirButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("Hola");
+			}
+		});
+		
+		eliminarButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("d");
+			}
+		});
+		
+		add(tablaPanel, BorderLayout.CENTER);
+		add(botonesPanel, BorderLayout.SOUTH);
+
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+
+		
+		
 		
 		this.setVisible(true);
 	}
