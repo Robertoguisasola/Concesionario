6dd71c6b6ce42f3cdd6fb7472869a8b970430d41
Roberto <Roberto@TorreBilbao>
2019-12-01 19:59:00 +0100

    Corrección VistaTrabajador

--- ProyectoConcesionario/src/ventanas/VistaTrabajador.java
+++ ProyectoConcesionario/src/ventanas/VistaTrabajador.java
@@ -1,17 +1,23 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import model.Trabajador;
 
@@ -24,13 +30,12 @@ public class VistaTrabajador extends JFrame{
 	
 	private JPanel panelSuperior;
 	private JPanel opcionesPanel;
-	private Box buttonsBox;
-	private JButton anadirCocheButton;
+	private Box opcionesBox;
+	private JButton cochesButton;
 	private JButton cerrarButton;
 	
 	public VistaTrabajador(Trabajador t) {
 		this.setTitle("Bienvenido "+ t.getNombre()+ " " + t.getApellidos());
-		this.setTitle("Bienvenido");
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(480,360);
@@ -56,8 +61,8 @@ public class VistaTrabajador extends JFrame{
 		opcionesPanel = new JPanel();
 		opcionesPanel.setLayout(new GridBagLayout());
 		
-		anadirCocheButton = new JButton("Ver coches");
-		anadirCocheButton.addActionListener(new ActionListener() {
+		cochesButton = new JButton("Ver coches");
+		cochesButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				TablaCoches tablaCoches = new TablaCoches(t);
 				tablaCoches.setLocationRelativeTo(null);
@@ -65,20 +70,15 @@ public class VistaTrabajador extends JFrame{
 				dispose();
 			}
 		});
-			
+		
+		opcionesBox = new Box(BoxLayout.Y_AXIS);
+		opcionesBox.add(cochesButton);
 		
 		
-		buttonsBox = new Box(BoxLayout.X_AXIS);
-		buttonsBox.add(anadirCocheButton);
-				
-		GridBagConstraints gbc_buttonsBox = new GridBagConstraints();
-		gbc_buttonsBox.gridx = 0;
-		opcionesPanel.add(buttonsBox, gbc_buttonsBox);
-		
+		opcionesPanel.add(opcionesBox);
+					
 		getContentPane().add(opcionesPanel, BorderLayout.CENTER);
 		
 		this.setVisible(true);
 	}
-	
-
 }
