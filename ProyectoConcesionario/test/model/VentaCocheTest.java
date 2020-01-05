523a015185260ddb4a6874a75f66784440c9c3cd
Robertoguisasola <robertomargui@yahoo.es>
2020-01-05 17:37:13 +0100

    Test de venta y ventacoches

---
+++ ProyectoConcesionario/test/model/VentaCocheTest.java
@@ -0,0 +1,116 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VentaCocheTest {
+	
+	//TODO tttt meter coches de 2ª mano, motos...
+	private Cliente c;
+	private Coche ch;
+	private Date fecha = new Date(0);
+	private VentaCoche v;
+	private VentaCoche prueba;
+	private VentaCoche empty;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		c = new Cliente("roberto", "1234", "robertomargui@yahoo.es", "71708119F", "Roberto", "Guisasola", fecha, 0000);
+		ch = new Coche("Volvo", "GG", Colores.VERDE, 200, 4, 5, true);
+		v = new VentaCoche(c, ch, 89000, "8219GZM", false, false, false, true, false);
+		prueba = new VentaCoche(v);
+		empty = new VentaCoche();
+	}
+
+	@Test
+	public void testIsTechoPanoramico() {
+		if (v.isTechoPanoramico()) {
+			fail("La venta no tiene el techo panorámico");
+		} else {
+			assertEquals(false, v.isTechoPanoramico());
+		}
+		
+		if (prueba.isTechoPanoramico()) {
+			fail("La venta no tiene el techo panorámico. Comprobar constructor copia");
+		} else {
+			assertEquals(false, v.isTechoPanoramico());
+		}
+		
+		if (empty.isTechoPanoramico()) {
+			fail("La venta no tiene el techo panorámico. Comprobar constructor vacío");
+		} else {
+			assertEquals(false, v.isTechoPanoramico());
+		}
+		
+		}
+
+	@Test
+	public void testSetTechoPanoramico() {
+		v.setTechoPanoramico(true);
+		if (!(v.isTechoPanoramico())) {
+			fail("La venta no tiene el techo panorámico");
+		} else {
+			assertEquals(true, v.isTechoPanoramico());
+		}
+	}
+
+	@Test
+	public void testIsTraccion4x4() {
+		if (!(v.isTraccion4x4())) {
+			fail("La venta tiene tracción 4X4");
+		} else {
+			assertEquals(true, v.isTraccion4x4());
+		}	
+		
+		if (!(prueba.isTraccion4x4())) {
+			fail("La venta tiene tracción 4X4. Comprobar constructor copia");
+		} else {
+			assertEquals(true, prueba.isTraccion4x4());
+		}
+		
+		if (empty.isTraccion4x4()) {
+			fail("La venta no tiene tracción 4X4. Comprobar constructor vacío");
+		} else {
+			assertEquals(false, empty.isTraccion4x4());
+		}
+	}
+
+	@Test
+	public void testSetTraccion4x4() {
+v.setTraccion4x4(false);
+assertEquals(false, v.isTraccion4x4());
+}
+
+	@Test
+	public void testIsModoDeportivo() {
+		if (v.isModoDeportivo()) {
+			fail("La venta no tiene modo deportivo");
+		} else {
+			assertEquals(false, v.isModoDeportivo());
+		}	
+		
+		if (prueba.isModoDeportivo()){
+			fail("La venta no tiene modo deportivo. Comprobar constructor copia");
+		} else {
+			assertEquals(true, prueba.isTraccion4x4());
+		}
+		
+		if (empty.isModoDeportivo()) {
+			fail("La venta no tiene modo deportivo. Comprobar constructor vacío");
+		} else {
+			assertEquals(false, empty.isTraccion4x4());
+		}
+}
+
+	@Test
+	public void testSetModoDeportivo() {
+		v.setModoDeportivo(true);
+		assertEquals(true, v.isModoDeportivo());
+		}
+}
---
+++ ProyectoConcesionario/test/model/VentaTest.java
@@ -0,0 +1,142 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VentaTest {
+
+	private Cliente c;
+	private Vehiculo ch;
+	private Date fecha = new Date(0);
+	private Venta v;
+	private Venta prueba;
+	private Venta empty;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		c = new Cliente("roberto", "1234", "robertomargui@yahoo.es", "71708119F", "Roberto", "Guisasola", fecha, 0000);
+		ch = new Vehiculo("Volvo", "GG", Colores.VERDE, 200, 4, 5);
+		v = new Venta(c, ch, 89000, "8219GZM", false, false);
+		prueba = new Venta(v);
+		empty = new Venta();
+	}
+
+	@Test
+	public void testGetComprador() {
+		assertEquals(c, v.getComprador());
+		assertEquals(c, prueba.getComprador());
+		assertEquals(null, empty.getComprador());
+	}
+
+	@Test
+	public void testSetComprador() {
+		Cliente cl = new Cliente("mikel", "1234", "mikel.romero@opendeusto.es", "20976509A", "Mikel", "Romero", fecha, 0);
+v.setComprador(cl);
+assertEquals(cl, v.getComprador());
+	}
+
+	@Test
+	public void testGetVehiculo() {
+assertEquals(ch, v.getVehiculo());
+assertEquals(ch, v.getVehiculo());
+assertEquals(null, empty.getVehiculo());
+}
+
+	@Test
+	public void testSetVehiculo() {
+Vehiculo vh = new Vehiculo("Nissan", "Juke", Colores.AZUL, 150, 4, 5);
+v.setVehiculo(vh);
+assertEquals(vh, v.getVehiculo());
+}
+
+	@Test
+	public void testGetPrecio() {
+		assertEquals(89000, v.getPrecio());
+		assertEquals(89000, v.getPrecio());
+		assertEquals(0, empty.getPrecio());
+		}
+
+	@Test
+	public void testSetPrecio() {
+v.setPrecio(84000);
+assertEquals(84000, v.getPrecio());
+}
+
+	@Test
+	public void testGetMatricula() {
+		assertEquals("8219GZM", v.getMatricula());
+		assertEquals("8219GZM", v.getMatricula());
+		assertEquals(null, empty.getMatricula());
+		}
+
+	@Test
+	public void testSetMatricula() {
+		v.setMatricula("1278DYS");
+		assertEquals("1278DYS", v.getMatricula());
+		}
+
+	@Test
+	public void testIsAutomatico() {
+		if (v.isAutomatico()) {
+			fail("El coche no es automático");
+		} else {
+assertEquals(false, v.isAutomatico());
+		}
+		
+		if (prueba.isAutomatico()) {
+			fail("El coche no es automático. Comprobar constructor copia");
+		} else {
+assertEquals(false, prueba.isAutomatico());
+		}
+		
+		if (empty.isAutomatico()) {
+			fail("El coche no es automático. Comprobar constructor vacío");
+		} else {
+assertEquals(false, empty.isAutomatico());
+		}
+	}
+
+	@Test
+	public void testSetAutomatico() {
+v.setAutomatico(true);
+assertEquals(true, v.isAutomatico());
+}
+
+	@Test
+	public void testIsLucesLed() {
+		if (v.isLucesLed()) {
+			fail("El coche no tiene luces led");
+		} else {
+assertEquals(false, v.isLucesLed());
+		}
+		
+		if (prueba.isLucesLed()) {
+			fail("El coche no tiene luces led. Comprobar constructor copia");
+		} else {
+assertEquals(false, prueba.isLucesLed());
+		}
+		
+		if (empty.isAutomatico()) {
+			fail("El coche no es automático. Comprobar constructor vacío");
+		} else {
+assertEquals(false, empty.isAutomatico());
+		}
+	}	
+
+	@Test
+	public void testSetLucesLed() {
+v.setLucesLed(true);
+assertEquals(true, v.isLucesLed());
+}
+
+	@Test
+	public void testToString() {
+assertEquals(c.toString() + " ha comprado " + ch.toString() + " por 89000€, con matrícula 8219GZM", v.toString());	}
+
+}
