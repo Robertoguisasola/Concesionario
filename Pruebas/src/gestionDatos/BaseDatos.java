f4bb0f0da867b0347fa8e44196fa83be5f0507f5
Robertoguisasola <44500206+Robertoguisasola@users.noreply.github.com>
2021-02-02 17:08:00 +0100

    1b y algo de 5

--- Pruebas/src/gestionDatos/BaseDatos.java
+++ Pruebas/src/gestionDatos/BaseDatos.java
@@ -2,11 +2,13 @@ package gestionDatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 
 import datos.Barco;
 import datos.Evento;
@@ -15,7 +17,7 @@ import datos.TipoEvento;
 
 public class BaseDatos {
 	private static Connection conexion;
-	
+
 	/** Abre conexión con la base de datos
 	 * @param nombreBD	Nombre del fichero de base de datos
 	 * @return	true si la conexión ha sido correcta, false en caso contrario
@@ -25,7 +27,7 @@ public class BaseDatos {
 			System.out.println( "Conexión abierta" );
 			Class.forName("org.sqlite.JDBC");  // Carga la clase de BD para sqlite
 			conexion = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
-			
+
 			// Creación de las tablas
 			Statement statement = conexion.createStatement();
 			String sent = "CREATE TABLE IF NOT EXISTS barco (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre varchar(100), codigo varchar(100), tipo varchar(100), email varchar(100), oceano varchar(100));";
@@ -33,14 +35,14 @@ public class BaseDatos {
 			sent = "CREATE TABLE IF NOT EXISTS evento (id INTEGER PRIMARY KEY AUTOINCREMENT, codigoBarco varchar(100), tipo varchar(100), fecha int);";
 			statement.executeUpdate( sent );
 			// fin creación bd
-			
+
 			return true;
 		} catch(Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
+
 	/** Cierra la conexión abierta de base de datos
 	 */
 	public static void cerrarConexion() {
@@ -51,7 +53,7 @@ public class BaseDatos {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/** Lee los barcos de la conexión de base de datos abierta
 	 * @return	Lista completa de barcos, null si hay algún error
 	 */
@@ -74,7 +76,7 @@ public class BaseDatos {
 			return null;
 		}
 	}
-	
+
 	/** Lee los eventos de la conexión de base de datos abierta
 	 * @return	Lista completa de eventos, null si hay algún error
 	 */
@@ -98,7 +100,7 @@ public class BaseDatos {
 			return null;
 		}
 	}
-	
+
 	/** Borra todo el contenido de la tabla eventos
 	 */
 	public static void resetEventos() {
@@ -109,15 +111,77 @@ public class BaseDatos {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/** Guarda un evento en la base de datos.
 	 * Lo inserta si no existe comprobando antes que el barco esté en su tabla correspondiente
 	 * @return	Devuelve true si es correcto y false si ha ocurrido algún error
 	 */
-	public static boolean addEvento(Evento e) {
+	public static boolean addEvento(Evento ev) {
 		//T1b - Carga de evento en la base de datos
+
+		String sql  = "INSERT INTO evento (codigoBarco, tipo, fecha)"
+				+ " VALUES (?,?,?)";
+
+		PreparedStatement stmt;
+		try {
+			stmt = conexion.prepareStatement(sql);
+
+			stmt.setString(1, ev.getCodigoBarco());
+			stmt.setString(2, ev.getTipo().toString());
+			stmt.setLong(3, ev.getFecha().getTimeInMillis());
+
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		return false;
 	}
-	
+
+	public static ArrayList<Evento> getEventosBarco(String b) {
+		try (Statement statement = conexion.createStatement()) {
+			ArrayList<Barco> barcos = new ArrayList<>();
+			Barco bb = null;
+			String sent = "select * from barco;";
+			System.out.println( sent );
+			ResultSet rs = statement.executeQuery( sent );
+			while( rs.next() ) { // Leer el resultset
+				String nombre = rs.getString("nombre");
+				String codigo = rs.getString("codigo");
+				String tipo = rs.getString("tipo");
+				String oceano = rs.getString("oceano");
+				barcos.add( new Barco ( nombre, null, codigo, TipoBarco.valueOf(tipo), oceano ) );
+			}
+
+			Iterator<Barco> bar= barcos.iterator();
+			while (bar.hasNext()) {
+				Barco b2 = bar.next();
+
+				if (b2.getNombre().equals(b)) {
+					bb = b2;
+				}
+			}
+
+			ArrayList<Evento> eventos = new ArrayList<>();
+			String sent2 = "select * from evento where codigoBarco = '" + bb.getCodigo() + "';";
+			System.out.println( sent2 );
+			ResultSet rs2 = statement.executeQuery( sent2 );
+
+			while( rs2.next() ) { // Leer el resultset
+				String codigoBarco = rs.getString("codigoBarco");
+				long fecha = rs.getLong("fecha");
+				GregorianCalendar cal = new GregorianCalendar();
+				cal.setTimeInMillis(fecha);
+				String tipo = rs.getString("tipo");
+				eventos.add( new Evento ( codigoBarco, cal, TipoEvento.valueOf(tipo) ) );
+			}
+			return eventos;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}	
 }
+
+
--- Pruebas/src/ventanas/VentanaDatos.java
+++ Pruebas/src/ventanas/VentanaDatos.java
@@ -1,6 +1,7 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JFrame;
@@ -15,14 +16,16 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
 import datos.Barco;
+import datos.Evento;
+import gestionDatos.BaseDatos;
 import principal.Examen;
 
 public class VentanaDatos extends JFrame {
 	DefaultTreeModel modelo;
 	DefaultMutableTreeNode abuelo;
-	
+
 	JTable tabla;
-	
+
 	public VentanaDatos() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
 		setTitle("Ventana de muestra de datos");
@@ -30,39 +33,47 @@ public class VentanaDatos extends JFrame {
 		JPanel pCentral = new JPanel();
 		pCentral.setLayout(new BorderLayout());
 		pCentral.add(new JLabel("Gestión de Barcos", SwingConstants.CENTER), BorderLayout.NORTH);
-		
+
 		abuelo = new DefaultMutableTreeNode("Gestion Barcos");
 		modelo = new DefaultTreeModel(abuelo);
 		JTree tree = new JTree(modelo);
 		cargarJTree();
-		
+
 		//Listener de selección de barco
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
-            public void valueChanged(TreeSelectionEvent e) {
-                DefaultMutableTreeNode node = (DefaultMutableTreeNode) e
-                        .getPath().getLastPathComponent();
-                if(e.getPath().getPathCount() == 3) {	//Si es un barco, lanzamos el evento
-                	String nodo = node.toString();
-                    System.out.println("Has seleccionado: " + nodo);
-                    
-                    //T5
+			public void valueChanged(TreeSelectionEvent e) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) e
+						.getPath().getLastPathComponent();
+				if(e.getPath().getPathCount() == 3) {	//Si es un barco, lanzamos el evento
+					String nodo = node.toString();
+					System.out.println("Has seleccionado: " + nodo);
 
-                                        
-                }
-            }
-        });
+					ArrayList<Evento> eventos = new ArrayList<Evento>();
+
+					BaseDatos.abrirConexion("gestionbarcos.db");
+					eventos = BaseDatos.getEventosBarco(node.toString());
+					BaseDatos.cerrarConexion();
+
+					for (Evento evento : eventos) {
+						System.out.println(evento.toString());
+					}
+
+
+				}
+			}
+		});
 		pCentral.add(tree, BorderLayout.WEST);
-		
+
 		//T5
-		
-		
-		
-		
+
+
+
+
 		add(pCentral);
-		
+
 		setVisible(true);
 	}
-	
+
 	public void cargarJTree() {
 		HashMap<String, DefaultMutableTreeNode> mapOceanos = new HashMap<>();
 		for (String o : Examen.getOceanos()) {
@@ -70,13 +81,13 @@ public class VentanaDatos extends JFrame {
 				DefaultMutableTreeNode nodo = new DefaultMutableTreeNode(o);
 				mapOceanos.put(o, nodo);
 				modelo.insertNodeInto(nodo,abuelo,0);
-				
+
 				for (Barco b : Examen.getBarcosOceano(o)) {
 					DefaultMutableTreeNode nodoHijo = new DefaultMutableTreeNode(b.getNombre());
 					modelo.insertNodeInto(nodoHijo,nodo,0);
 				}
 			}
 		}
-		
+
 	}
 }
